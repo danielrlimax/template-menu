a772130cf755a61a40b160639a95f0bf4ca844af
Daniel de lima <danielrlima@proton.me>
2026-05-21 22:27:13 -0300

    Feat: Dados do Banco Criado

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -44,7 +44,7 @@ export default function Header({ onCartClick, config }: HeaderProps) {
                   <img 
                     src={config.logoImage} 
                     alt={config.name} 
-                    className="w-full h-full object-contain p-1"
+                    className="w-full h-full object-contain "
                   />
                 ) : (
                   <span className="text-white font-extrabold text-lg sm:text-xl">
